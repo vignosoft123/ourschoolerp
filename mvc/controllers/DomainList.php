755f140af1dcf154e3f4aea61b5de161195edd2d
srinivas <ssvasu4u@gmail.com>
2025-06-30 01:46:20 +0530

    d

--- mvc/controllers/DomainList.php
+++ mvc/controllers/DomainList.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Domainlist extends CI_Controller {
+class DomainList extends CI_Controller {
 
     public function index() {
         $file_path = FCPATH . 's.txt';
